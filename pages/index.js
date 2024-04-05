6609f522c9363a6ff4a7ff30a393ece944859802
robin.mathew <robin.mathew@experionglobal.com>
2024-04-05 16:53:03 +0530

    index page updated

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
+import Head from "next/head";
+import Link from "next/link";
+
 function HomePage() {
-  return <div>Welcome to Next.js!</div>;
+  return (
+    <>
+      <Head>
+        <title>My Music Store</title>
+      </Head>
+      <div>
+        <h1>Welcome to Next.js!</h1>
+        <Link href="posts/firstPost">First Post</Link>
+      </div>
+    </>
+  );
 }
 
 export default HomePage;
